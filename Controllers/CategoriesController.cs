3f080199e344a268360660beb37e22e414fa541b
Emy Mahmoud <emyashraf@Mahmouds-MacBook-Pro.local>
2025-01-09 15:32:58 +0200

    Initial commit

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+namespace WebApp.Controllers;
+
+public class CategoriesController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        var categories = CategoriesRepository.GetCategories();
+        return View(categories);
+    }
+
+    public IActionResult Edit(int? id)
+    {
+        var category = CategoriesRepository.GetCategoryById(id.HasValue?id.Value:0);
+                return View(category);
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,8 @@
+namespace WebApp.Models;
+
+public class Category
+{
+    public int CategoryId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
